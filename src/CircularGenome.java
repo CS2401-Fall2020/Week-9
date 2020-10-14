5a711c381d52d7ebe42efabea6f6f17116053be8
Dan DeBlasio <dan@dandeblasio.com>
2020-10-14 15:33:10 -0600

    Update CircularGenome.java

---
+++ src/CircularGenome.java
@@ -0,0 +1,26 @@
+
+public class CircularGenome {
+  // two Nucleotide pointers for head and tail
+
+  // constructor populates the head and tail pointer with Nucleotides
+  // should use charAt and toUpperCase
+  CircularGenome(String inGenome){
+  }
+  
+  // output a linearized version of the circular genome, use the pointers to know when youve made one rotation
+  // no need to change case here, will always output in all caps
+  public String toString() {
+    return null; // remove this line
+  }
+  
+  /* checks each position as a possible start location either returns:
+   *    "'<all caps search>' found at position(s) #, #...." 
+   *         or 
+   *   "'<all caps search>' not found"
+   * 
+   *    should use toUpperCase and match but no need for charAt in this method
+   */
+  public String search(String inSequence) {
+    return null; // remove this line
+  }
+}
