56af6af681355f2322cdc3f878be8e7305d7fb51
Dan DeBlasio <dan@dandeblasio.com>
2020-10-14 15:33:51 -0600

    Update Nucleotide.java

---
+++ src/Nucleotide.java
@@ -0,0 +1,15 @@
+
+public class Nucleotide {
+  // Class variables: (private char, public Nucleotide)
+  
+  // Constructor
+
+  
+  // Getter
+  
+  // recursive match
+  //   given a string match the first char if possible, then recursively call on rest of list
+  public boolean match(String inSequence) {
+    return false; // remove this line
+  }
+}
